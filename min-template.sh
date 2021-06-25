c5a16232e7588198e064dc2ed5bb6e21bba29eb1
Alin Mr <almr.oss@outlook.com>
2021-06-25 05:23:22 +0300

    min-template.sh: -- 'sed \+', vim bash->sh note

--- min-template.sh
+++ min-template.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #test $(( BASH_VERSINFO[0] > 4 || (BASH_VERSINFO[0] == 4 && BASH_VERSINFO[1] >= 4) )) -eq 1 || exit 1
+### sed -e '/##bash:/d' -e '/##sh:/s/^\([[:space:]]*\)#*/\1/' <min-template.sh  # bash -> sh
+### vim bash -> sh: g/##bash:$/d | %s/^\(\s\+\)##\(.\{-}\)\s\+##sh:$/\1\2/
+### (. ~/src/bashaaparse/min-template.sh; __parse_args -v --test-opt=x 1 2)  # test
 
 __usage() {  # args: header footer
   local flags
   flags=$(declare -p | sed -Ene 's/^declare -\S*\s+_O_([^=]+)=.*/\1/p')  ##bash:
-  ##flags=$(set | sed -ne 's/^_O_\([^=]\+\)=.*/\1/p')  ##sh:
+  ##flags=$(set | sed -ne 's/^_O_\([^=]*\)=.*/\1/p')  ##sh:
   printf '%s'${1:+'\n'}  "${1:-}"  # add \n only if missing
   test -z "$flags" || printf -- '--%s=ARG\n'  $(printf '%s'  "$flags" | sed -e 's/_/-/g')
   printf '%s'${2:+'\n'}  "${2:-}"
@@ -18,7 +21,8 @@ __parse_args() {
       -h|--help|--usage|-'?') __usage 'Options:'; exit 0 ;;
       --*=*)
         k=${1%%=*}; k=_O_${k#--}; printf -v "${k//-/_}" '%s'  "${1#--*=}"  ##bash:
-        ##k=${1%%=*}; k=_O_$(echo "${k#--}" | sed -e 's/-/_/g'); eval "$k"'=$(printf "%sX"  "${1#--*=}");' "$k=\${$k%X}"  ##sh:
+        ##k=${1%%=*}; k=_O_$(echo "${k#--}" | sed -e 's/-/_/g')  ##sh:
+        ##eval "$k"'=$(printf "%sX"  "${1#--*=}");' "$k=\${$k%X}"  ##sh:
         ;;
       --exit) return 0 ;;
       --no-?*) k=$1; shift; __parse_args "--${k#--no-}=false" "$@"; return ;;
@@ -38,5 +42,3 @@ __process_arg() {  # if $1 handled, return 0; exit to stop processing
   return 1;
 }
 ### end override
-### (. ~/src/bashaaparse/min-template.sh; __parse_args -v --test-opt=x 1 2)  # test
-### sed -e '/##bash:/d' -e '/##sh:/s/^\([[:space:]]*\)#*/\1/' <min-template.sh  # generate sh version
