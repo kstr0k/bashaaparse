2f6f94f5d07becf75e98ca2e6bdaa550f9844ba6
Alin Mr <almr.oss@outlook.com>
2021-08-05 14:08:51 +0300

    min-template.sh: don't sed in parsing; sh self-gen

--- min-template.sh
+++ min-template.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #test $(( BASH_VERSINFO[0] > 4 || (BASH_VERSINFO[0] == 4 && BASH_VERSINFO[1] >= 4) )) -eq 1 || exit 1
-### sed -e '/##bash:/d' -e '/##sh:/s/^\([[:space:]]*\)#*/\1/' <min-template.sh  # bash -> sh
+### sed -e '/##bash:/d' -e '/##sh:/{s/^\([[:space:]]*\)#*/\1/;s/[[:space:]]*##*sh:$//}' <min-template.sh  # bash -> sh
 ### vim bash -> sh: g/##bash:$/d | %s/^\(\s\+\)##\(.\{-}\)\s\+##sh:$/\1\2/
 ### (. ~/src/bashaaparse/min-template.sh; __parse_args -v --test-opt=x 1 2)  # test
 
 __usage() {  # args: header footer
   local flags
-  flags=$(declare -p | sed -Ene 's/^declare -\S*\s+_O_([^=]+)=.*/\1/p')  ##bash:
-  ##flags=$(set | sed -ne 's/^_O_\([^=]*\)=.*/\1/p')  ##sh:
+  flags=$(declare -p | sed -ne 's/^declare -[^[:space:]]*[[:space:]]*_O_\([^=]*\)=.*/--\1/; t p; d; :p; s/_/-/g; p;')  ##bash:
+  ##flags=$(set | sed -ne 's/^_O_\([^=]*\)=.*/--\1/; t p; d; :p; s/_/-/g; p')  ##sh:
   printf '%s'${1:+'\n'}  "${1:-}"  # add \n only if missing
-  test -z "$flags" || printf -- '--%s=ARG\n'  $(printf '%s'  "$flags" | sed -e 's/_/-/g')
+  test -z "$flags" || printf '%s=ARG\n'  $flags
   printf '%s'${2:+'\n'}  "${2:-}"
 }
 __parse_args() {
@@ -19,10 +19,12 @@ __parse_args() {
     case "$1" in
       -v) set -x ;;
       -h|--help|--usage|-'?') __usage 'Options:'; exit 0 ;;
-      --*=*)
-        k=${1%%=*}; k=_O_${k#--}; printf -v "${k//-/_}" '%s'  "${1#--*=}"  ##bash:
-        ##k=${1%%=*}; k=_O_$(echo "${k#--}" | sed -e 's/-/_/g')  ##sh:
-        ##eval "$k"'=$(printf "%sX"  "${1#--*=}");' "$k=\${$k%X}"  ##sh:
+      --*=*) k=${1%%=*}; k=_O_${k#--}
+        printf -v "${k//-/_}" '%s'  "${1#--*=}"  ##bash:
+        ##k=$k-; while :; do case "$k" in  ##sh:
+        ##  *-) k=${k%%-*}_${k#*-} ;;  ##sh:
+        ##   *) eval "${k%_}=\${1#--*=}"; break ;;  ##sh:
+        ##esac; done  ##sh:
         ;;
       --exit) return 0 ;;
       --no-?*) k=$1; shift; __parse_args "--${k#--no-}=false" "$@"; return ;;
@@ -36,7 +38,7 @@ __parse_args() {
 
 ### override these
 __main() {
-  __usage 'Options:' "Args: $(test $# -eq 0 || /usr/bin/env printf '%q ' "$@")" 1>&2
+  __usage 'Options:' "Args: $(test $# -eq 0 || printf '{%s} ' "$@")" 1>&2
 }
 __process_arg() {  # if $1 handled, return 0; exit to stop processing
   return 1;
