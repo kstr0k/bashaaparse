502bbd7c5f065d93d9ba1237d344fd6f6b3c4733
Alin Mr <almr.oss@outlook.com>
2021-08-10 07:47:14 +0300

    min-template.sh: -? = defaults; __usage API change

--- min-template.sh
+++ min-template.sh
@@ -4,19 +4,21 @@
 ### bash -uec '. ./min-template.sh; __parse_args ----gen=sh-strip >mt.sh'  # generate sh version
 #shellcheck disable=SC3028,SC3043
 
-__usage() {  # args: header footer
-  local flags p='/^_O_/!d;/[^[:alnum:]_]/d;s/$/=ARG/;s/_/-/g;s/^-O-/--/p'; flags=$(
-    #{ [ -z "${BASH_VERSION:-}" ] || compgen -v; [ -z "${ZSH_VERSION:-}" ] || emulate zsh -c 'zmodload zsh/parameter; print -rl -- ${(k)parameters}'; } | sed -n -e "$p")  ##bash:
-    set | sed -n -e '/=/!d;s/=.*//' -e "$p")  ##sh:
-  printf '%s'${1:+'\n'}  "${1:-}"  # add \n only if missing
-  test -z "$flags" || printf '%s\n' "$flags"
+__usage() {  # args: <help-option> header footer
+  local vs=false; [ "$1" != '-?' ] || vs=true; shift
+  local flags p='/^_O_/!d;/[^[:alnum:]_]/d;h;s/_/-/g;s/^-O-/--/;s/$/=ARG/p'
+  ! "$vs" || p=${p%';s'*}';G;s/\n/[=$/;s/$/]/p'
+  #flags=$({ [ -z "${BASH_VERSION:-}" ] || compgen -v; [ -z "${ZSH_VERSION:-}" ] || emulate zsh -c 'zmodload zsh/parameter; print -rl -- ${(k)parameters}'; } | sed -n -e "$p")  ##bash:
+  flags=$(set | sed -n -e '/=/!d;s/=.*//' -e "$p")  # posh fails: no =.*  ##sh:
+  printf '%s'${1:+'\n'}  "${1:-}"
+  test -z "$flags" || { ! "$vs" || eval flags="\"$flags\""; printf '%s\n' "$flags"; }
   printf '%s'${2:+'\n'}  "${2:-}"
 }
 __parse_args() {
   local k; test $# -gt 0 || set -- --
   if ! __process_arg "$@"; then case "$1" in
       -v) set -x ;;
-      -h|--help|--usage|-'?') __usage 'Options:'; exit 0 ;;
+      -h|--help|--usage|-'?') __usage "$1" 'Options:'; exit 0 ;;
       --*=*) k=${1%%=*}; k=_O_${k#--}
         #printf -v "${k//-/_}" '%s'  "${1#--*=}" ;;  ##bash:
         k=$k-; while :; do case "$k" in  ##sh:
@@ -45,7 +47,7 @@ __parse_args_debug_main() {
       if $strip; then s1="/##$osh:"'$/d'; s2=''; fi
       (set -x; sed <"$src" -n -e '1{s@^\(#!\).*'"@\1/bin/$sh@p;d;}" -e "$s1" -e 's/\([[:space:]]*##'"$sh"':\)$/'"$s2"'/' -e ts -e 'p;d' -e :s -e 's/^\([[:space:]]*\)#*/\1/;p')
   fi
-  __usage 'Options:' "Args:$(test $# -eq 0 || printf ' {%s}' "$@")" 1>&2
+  __usage '' 'Options:' "Args:$(test $# -eq 0 || printf ' {%s}' "$@")" 1>&2
 }
 __process_arg() { return 1; }  # if $1 handled, return 0; exit to stop processing
 __main() { __parse_args_debug_main "$@"; }
